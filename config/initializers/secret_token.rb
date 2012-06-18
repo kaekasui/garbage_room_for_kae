8871ff155b2b0f1dc2046bbc11607b95956083c0
mayu <white_hot_chocolate@i.softbank.jp>
2012-06-18 23:31:05 +0900

    fixes #1 : The rails project 'kaenoheya' was created.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kaenoheya::Application.config.secret_token = 'b389a0257f134e19e5367863d66e7570be98ba65940f22ebbc9bcf60eab371d0d4bea5960cfb9764d5f307c6cd9b548b8284ce9036941495f02d9686b101567e'
