66aecfc4158e677fd4c8c7bfa59cfb5db3271ff9
mayu <white_hot_chocolate@i.softbank.jp>
2012-06-24 13:53:01 +0900

    refs #11 : The table of updating information is added to the top.

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,7 @@
 class TopController < ApplicationController
 
   def index
+    @updated_informations = UpdatedInformation.no_draft.limit3
+    respond_to {|format| format.html}
   end
 end
--- app/models/updated_information.rb
+++ app/models/updated_information.rb
@@ -1,4 +1,6 @@
 class UpdatedInformation < Admin::UpdatedInformation
   attr_accessible :comment, :deleted_at, :display_at, :draft, :news, :title, :version_id
 
+  scope :no_draft, where("draft != true")
+  scope :limit3, limit(3)
 end
