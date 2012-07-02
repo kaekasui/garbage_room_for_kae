ba1a3d8f4092094e8c0c4f9535401133a3185641
mayu <white_hot_chocolate@i.softbank.jp>
2012-07-02 13:07:45 +0900

    refs #34 : 'rvmsudo rake db:migrate'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120627233913) do
+ActiveRecord::Schema.define(:version => 20120702035826) do
 
   create_table "blogs", :force => true do |t|
     t.string   "title"
@@ -36,6 +36,16 @@ ActiveRecord::Schema.define(:version => 20120627233913) do
     t.datetime "updated_at",        :null => false
     t.string   "mini_label"
     t.string   "mini_label_option"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "screen_name"
+    t.string   "image"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
 end
