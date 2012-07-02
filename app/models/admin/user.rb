8041634aa72bd0e95ecff6b146c0a3fdd323724b
mayu <white_hot_chocolate@i.softbank.jp>
2012-07-02 19:44:21 +0900

    refs #34 : Twitter連携の実装

--- app/models/admin/user.rb
+++ app/models/admin/user.rb
@@ -2,4 +2,18 @@ class Admin::User < ActiveRecord::Base
   # attr_accessible :title, :body
   set_table_name :users
 
+  def self.create_with_omniauth(auth)
+    user = User.new
+    user[:provider] = auth["provider"]
+    user[:uid] = auth["uid"]
+
+    unless auth["info"].blank?
+      user[:name] = auth["info"]["name"]
+      user[:screen_name] = auth["info"]["nickname"]
+      user[:image] = auth["info"]["image"]
+    end
+
+    user.save
+    return user
+  end
 end
