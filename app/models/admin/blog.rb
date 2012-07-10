a60b6463e83bbf68433ae63af202d897c339670e
mayu <white_hot_chocolate@i.softbank.jp>
2012-07-11 04:19:47 +0900

    refs #40 : 空・文字数超過によるエラーの表示

--- app/models/admin/blog.rb
+++ app/models/admin/blog.rb
@@ -2,4 +2,8 @@ class Admin::Blog < ActiveRecord::Base
   set_table_name :blogs
 
   attr_accessible :created_at, :blog_category_code, :contents1, :contents2, :deleted_at, :draft, :title
+
+  validates_presence_of :title, :contents1
+  validates_length_of :title, maximum: AVAILABLE_STRING_LENGTH, size: AVAILABLE_STRING_LENGTH
+  validates_length_of :contents1, :contents2, maximum: AVAILABLE_LONG_STRING_LENGTH, size: AVAILABLE_TEXT_LENGTH
 end
