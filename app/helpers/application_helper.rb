368c33e62c0ed8dfd1f1fefef77d63652c6b39a9
mayu <white_hot_chocolate@i.softbank.jp>
2012-07-11 05:04:02 +0900

    fixes #40 : 入力必須項目に赤文字*を追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,11 @@ module ApplicationHelper
   def to_br(text)
     text.gsub(/\r\n|\r|\n/, "<br />")
   end
+
+  # 入力必須の項目であった場合に「*」を表示する
+  def mark_required(object, attribute)
+    mark = raw "<span class='require_mark'>*</span>"
+    mark if object.class.validators_on(attribute).map(&:class).include? ActiveModel::Validations::PresenceValidator
+  end
+
 end
