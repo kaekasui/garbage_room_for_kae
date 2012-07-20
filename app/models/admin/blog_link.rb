41750d62381184f00891a8bdd0c26c0a6551a64d
mayu <white_hot_chocolate@i.softbank.jp>
2012-07-21 04:04:20 +0900

    fixes #56 : エラー処理を追加

--- app/models/admin/blog_link.rb
+++ app/models/admin/blog_link.rb
@@ -2,4 +2,8 @@ class Admin::BlogLink < ActiveRecord::Base
   set_table_name :blog_links
 
   attr_accessible :author, :deleted_at, :draft, :introduction, :position, :title, :url
+
+  validates_presence_of :title, :author, :url
+  validates_length_of :title, :author, :introduction, :url, maximum: AVAILABLE_STRING_LENGTH, size: AVAILABLE_STRING_LENGTH
+
 end
