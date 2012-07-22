1dbc153a56a8a44dad8402ac5f84b0c6f845a85d
mayu <white_hot_chocolate@i.softbank.jp>
2012-07-22 19:09:24 +0900

    refs #62 : テーブル名のセット

--- app/models/admin/version.rb
+++ app/models/admin/version.rb
@@ -1,3 +1,5 @@
 class Admin::Version < ActiveRecord::Base
+  set_table_name :versions
+
   attr_accessible :deleted_at, :flag, :name
 end
--- app/models/version.rb
+++ app/models/version.rb
@@ -1,3 +1,3 @@
-class Version < ActiveRecord::Base
+class Version < Admin::Version
   attr_accessible :deleted_at, :flag, :name
 end
