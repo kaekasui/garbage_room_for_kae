e1e00a05a22fb3dcc7c0b1511fe100036926de2b
mayu <white_hot_chocolate@i.softbank.jp>
2012-10-10 08:27:27 +0900

    fixes #72 : 現バージョンの表示

--- app/models/version.rb
+++ app/models/version.rb
@@ -1,3 +1,8 @@
 class Version < Admin::Version
   attr_accessible :deleted_at, :flag, :name
+
+  def self.now
+    now_version = Version.find_by_flag(true)
+    now_version.name unless now_version.nil?
+  end
 end
