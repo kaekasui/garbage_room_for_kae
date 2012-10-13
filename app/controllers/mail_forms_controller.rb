45dea791c1ca69a3e5f6368c10dfb059eb494cf8
mayu <white_hot_chocolate@i.softbank.jp>
2012-10-13 14:06:31 +0900

    refs #73 : メール送信機能の実装

---
+++ app/controllers/mail_forms_controller.rb
@@ -0,0 +1,19 @@
+class MailFormsController < ApplicationController
+
+  # POST /mail_forms
+  # POST /mail_forms.json
+  def create
+    @mail_form = Admin::MailForm.new(params[:mail_form])
+
+    respond_to do |format|
+      if @mail_form.save
+        format.html { redirect_to blogs_path(anchor: "mail_form"), notice: I18n.t("flash.create_mail_form")}
+        format.json { render json: @mail_form, status: :created, location: @mail_form }
+      else
+        #format.html { render action: "new" }
+        format.html { redirect_to blogs_path(anchor: "mail_form"), notice: I18n.t("flash.cannot_create_mail_form") }
+        format.json { render json: @mail_form.errors, status: :unprocessable_entity }
+      end 
+    end 
+  end 
+end
--- app/models/admin/mail_form.rb
+++ app/models/admin/mail_form.rb
@@ -1,3 +1,7 @@
 class Admin::MailForm < ActiveRecord::Base
   set_table_name :mail_forms
+
+  attr_accessible :name, :mail, :content
+
+  validates_presence_of :content
 end
