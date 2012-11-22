41cb820b7609829b0ec68f650f4f17d31f401593
mayu <white_hot_chocolate@i.softbank.jp>
2012-11-23 00:06:39 +0900

    fixes #88 : データ保存時にhostname取得

--- app/models/blog_comment.rb
+++ app/models/blog_comment.rb
@@ -1,11 +1,24 @@
 class BlogComment < Admin::BlogComment
   attr_accessible :admin, :author, :blog_id, :content, :draft, :host, :ip, :mail, :mixi, :password, :salt, :title, :url
 
+  before_save :set_hostname
+
   def self.recent
     self.limit(5)
   end
 
   def blog_title
     Blog.find(self.blog_id).title if Blog.find(self.blog_id)
+  end
+
+  def hostname(ip)
+     hostname = Socket.getnameinfo([nil, nil, nil, ip]) 
+     hostname[0]
+  end
+
+  private
+
+  def set_hostname
+    self.host = hostname(self.ip)
   end
 end
