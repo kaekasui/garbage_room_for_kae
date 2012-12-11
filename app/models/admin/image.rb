c6c9f3525415560c6f5d639d6abda7d15f6e7346
mayu kojima <mayu@mayu-kojima-no-MacBook-Pro.local>
2012-12-11 09:47:32 +0900

    refs #111 : 不要になったadmin/imageを削除'rails destroy scaffold admin/image'

--- app/models/admin/image.rb
+++
@@ -1,8 +0,0 @@
-class Admin::Image < ActiveRecord::Base
-  attr_accessible :name
-
-  has_attached_file :avatar,
-    :storage => :dropbox,
-    :dropbox_credentials => "#{Rails.root}/config/dropbox.yml"#,
-    #:dropbox_options => {...}
-end
--- db/migrate/20121206133409_create_admin_images.rb
+++
@@ -1,9 +0,0 @@
-class CreateAdminImages < ActiveRecord::Migration
-  def change
-    create_table :admin_images do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
