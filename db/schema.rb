e48d5333471e1b0e0f491562b8b18b081b829396
mayu <white_hot_chocolate@i.softbank.jp>
2013-03-12 20:25:02 +0900

    refs #127 : ブログ本文の背景に色を追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121206133409) do
-
-  create_table "admin_images", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
+ActiveRecord::Schema.define(:version => 20121010232700) do
 
   create_table "admin_versions", :force => true do |t|
     t.datetime "created_at", :null => false
